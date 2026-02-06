4a4ad87d5e155705094b95f0277c692e14218d88
AlphaTrauma <deloriet@gmail.com>
2026-02-07 00:10:30 +0500

    added .env

--- lib/domain/quiz_service.dart
+++ lib/domain/quiz_service.dart
@@ -1,16 +1,17 @@
 import 'package:dio/dio.dart';
 import 'package:quizapp/domain/models/category.dart';
 import 'package:quizapp/domain/models/question.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class QuizService {
-  static const String _apiKey = 'ns8aeZOHHDkGLKz7bfoCdMpDO5fjLSxHqzZQdj2i';
+  static String get apiKey => dotenv.env['QUIZ_API_KEY'] ?? '';
   final Dio _dio = Dio();
 
   Future<List<Category>> fetchCategories() async {
     try {
       final response = await _dio.get(
         'https://quizapi.io/api/v1/categories',
-        options: Options(headers: {'X-Api-Key': _apiKey}),
+        options: Options(headers: {'X-Api-Key': apiKey}),
       );
 
       final List<dynamic> data = response.data as List<dynamic>;
@@ -30,7 +31,7 @@ class QuizService {
         'category': categoryName,
         'limit': limit,
       },
-      options: Options(headers: {'X-Api-Key': _apiKey}),
+      options: Options(headers: {'X-Api-Key': apiKey}),
     );
 
     if (response.data is! List) {
